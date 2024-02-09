4c19a481d478516f124e96299466ea36cb7bc229
yyyayee <153577004+yyyayee@users.noreply.github.com>
2024-02-09 11:33:38 +0700

    Lab 03 - Build an On Boarding Screen Mobile App

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'screen/introduction_screen.dart';
+
+bool show = true;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  show = prefs.getBool('ON_BOARDING') ?? true;
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Onboarding Screen',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: IntroScreen(),
+    );
+  }
+}
